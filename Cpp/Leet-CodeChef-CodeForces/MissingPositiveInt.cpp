e6ab8b092b566eb08f8f8a15aa27ce63bebcf6b0
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-03-26 18:39:31 +0530

    Optimum Approach of Missing Posiitive

--- Cpp/Leet-CodeChef-CodeForces/MissingPositiveInt.cpp
+++ Cpp/Leet-CodeChef-CodeForces/MissingPositiveInt.cpp
@@ -1,3 +1,57 @@
+/*
+Optimum Approach - 
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        bool contains1 = false;
+
+        // Replace negative numbers, zeros,
+        // and numbers larger than n with 1s.
+        // After this nums contains only positive numbers.
+        for (int i = 0; i < n; i++) {
+            // Check whether 1 is in the original array
+            if (nums[i] == 1) {
+                contains1 = true;
+            }
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
+            }      
+        }
+
+        if (!contains1) return 1;
+
+        // Mark whether integers 1 to n are in nums
+        // Use index as a hash key and negative sign as a presence detector.
+        for (int i = 0; i < n; i++) {
+            int a = abs(nums[i]);
+            if (a == n) {
+                nums[0] = -abs(nums[0]);
+            } else {
+                nums[a] = -abs(nums[a]);
+            }       
+        }
+
+        // First positive in nums is smallest missing positive integer
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        // nums[0] stores whether n is in nums
+        if (nums[0] > 0) {
+            return n;
+        }
+
+        // If nums contained all elements 1 to n
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+};
+
+*/
+
+
 #include <iostream>
 #include <bits/stdc++.h>
 #include <algorithm>

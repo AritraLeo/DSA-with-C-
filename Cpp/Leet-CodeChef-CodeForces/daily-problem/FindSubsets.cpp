cf7f471d9bb95895827045299667692ea79d8acd
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-05-21 22:16:54 +0530

    Logic of all subsets

--- Cpp/Leet-CodeChef-CodeForces/daily-problem/FindSubsets.cpp
+++ Cpp/Leet-CodeChef-CodeForces/daily-problem/FindSubsets.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+/*
+Logic -
+
+The subsets function initializes an empty result vector to store all the subsets and an empty subset vector to keep track of the current subset being generated.
+It calls the backtrack function with the initial parameters: result, subset, nums, and start index 0.
+In the backtrack function:
+It adds the current subset to the result vector.
+It iterates through the remaining elements in nums starting from the start index.
+For each element, it adds the element to the subset, recursively calls backtrack with the updated start index, and then removes the element from the subset to backtrack.
+The subsets function returns the result vector containing all the generated subsets.
+*/
+
 class Solution
 {
 public:
